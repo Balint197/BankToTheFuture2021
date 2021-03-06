bde3c583d3c867161ed6eec2c892d5128a443b3f
Krisz98 <kkranitz98@gmail.com>
2021-03-06 14:15:43 +0100

    checkpoint commit
    Buffet class extended

--- controller.gd
+++ controller.gd
@@ -9,6 +9,7 @@ var buffetClass = preload("Buffet.gd")
 onready var UI = get_tree().get_root().get_node("UI")
 onready var money = get_tree().get_root().get_node("UI/TextureRect/Label")
 onready var priceSlider = get_tree().get_root().get_node("UI/ColorRect/HScrollBar")
+onready var uiInterface = get_tree().get_root().get_node("UI/ColorRect")
 
 onready var dayTimer = get_tree().get_root().get_node("UI/controller/DayTimer")
 #---------------------------END OF ACCESSING NODES-----------------------------------------------------------#
@@ -21,10 +22,12 @@ enum {START, DAY_INIT, DAY_RUNNING, DAY_ELAPSED, BETWEEN_DAYS}
 #variable for storing the main state of the application lifecycle
 var mainState = START
 
+var start_to_dayInit := false
+
 #variables for state transitions
 var isDayElapsed := false
 
-var myBuffet : Buffet
+var myBuffet : Buffet = null
 
 #---------------------------END OF DECLARING VARIABLES-------------------------------------------------------#
 
@@ -39,7 +42,18 @@ func _process(delta):
 	
 	pass
 
-
+func collectUIData():
+	var uiData = uiInterface.reportValues()
+	print(myBuffet)
+	if myBuffet != null:
+		print("NOTNULL")
+		#  [ár 0...100, bérelt hely 0...3, marketing 0...100, chef 1...4, alapanyag 0...4]
+		print(uiData[0])
+		myBuffet.currentPrice =  uiData[0]
+		myBuffet.rentedSpace  =  uiData[1]
+		myBuffet.marketing =     uiData[2]
+		myBuffet.chefs =         uiData[3]
+		myBuffet.rawMaterial =   uiData[4]
 
 func doStateTransitions():
 	var nextState = mainState
@@ -47,7 +61,8 @@ func doStateTransitions():
 	#ONLY HANDLE STATE TRANSITIONS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	match mainState:
 		START:
-			nextState = DAY_INIT
+			if start_to_dayInit:
+				nextState = DAY_INIT
 			print("nextState")
 			print(nextState)
 		DAY_INIT:
@@ -84,11 +99,15 @@ func _on_FrameUpdateTimer_timeout():
 			print(mainState)
 			#init the bufet
 			myBuffet = buffetClass.new()
-			
+			start_to_dayInit = true
 		DAY_INIT:
+			#start the day
 			dayTimer.start()
 			print(mainState)
 		DAY_RUNNING:
+			#collect ui data
+			collectUIData()
+			
 			print(mainState)
 		DAY_ELAPSED:
 			print(mainState)
