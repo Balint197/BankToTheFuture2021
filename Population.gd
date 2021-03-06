0538c34c66072edac5cc0577b9526378ed616373
bcsabae <berky.csaba@outlook.com>
2021-03-06 16:19:10 +0100

    Added corrections after initial testing

--- Population.gd
+++ Population.gd
@@ -16,17 +16,20 @@ var active_customers = []
 #list of passive customers
 var passive_customers = []
 
+#copy of marketing value from UI
+var marketing
+
 #total number of population
-const POPULATION = 10000
+const POPULATION = 100
 
 const DEBUG = true
 
 #instances of customer classes for calculations
 var customerTypes := {
-	'POOR': Customer.new('CUSTOMER_0'),
-	'MIDDLE_LOW': Customer.new('CUSTOMER_1'),
-	'MIDDLE_HIGH': Customer.new('CUSTOMER_2'),
-	'RICH': Customer.new('CUSTOMER_3')
+	'POOR': Customer.new('POOR'),
+	'MIDDLE_LOW': Customer.new('MIDDLE_LOW'),
+	'MIDDLE_HIGH': Customer.new('MIDDLE_HIGH'),
+	'RICH': Customer.new('RICH')
 }
 
 func get_amount_for_type(price, customer_type):
@@ -39,14 +42,21 @@ func get_amount_for_type(price, customer_type):
 				'm': get_population_reach(),
 				'c': POPULATION * population_ratios[customer_type] * get_population_reach()
 			}))
-		return POPULATION * population_ratios[customer_type] * get_population_reach()
+		return round(POPULATION * population_ratios[customer_type] * get_population_reach())
 	else:
 		return 0
 
+#return a dictionary containing cusomer amounts for each category with a given price
+func get_amount_for_all(price):
+	var amount_for_types = {}
+	for key in customerTypes:
+		amount_for_types[key] = get_amount_for_type(price, key)
+	return amount_for_types
+
 #get reach from UI, normalize for [0,1]
 func get_population_reach():
-	var marketing = get_tree().get_root().get_node("UI/ColorRect").reportValues()['marketing']
-	return marketing/100
+	#var marketing = get_tree().get_root().get_node("UI/ColorRect").reportValues()[2]
+	return self.marketing/100.0
 
 #initialize active customers array
 #input: customers as a dictionary: 'name':amount
@@ -54,23 +64,32 @@ func init_active_customers(customers, price):
 	if (!active_customers.empty()):
 		delete_customer_array(true)
 	for type in customers:
-		var amount = get_amount_for_type(price, key)
+		var amount = get_amount_for_type(price, type)
 		for i in range(0, amount):
-			active_customers.push_back(Customer.new(key))
+			active_customers.push_back(Customer.new(type))
 	return
 
 #delete array objects individually to avoid memory leak
 func delete_customer_array(active):
 	if active:
-		for i in range(0, active_customers.count()):
+		for i in range(0, active_customers.size()):
 			active_customers[i].free()
-			active_customers.clear()
+		active_customers.clear()
 	else:
-		for i in range(0, passive_customers.count()):
+		for i in range(0, passive_customers.size()):
 			passive_customers[i].free()
-			passive_customers.clear()
+		passive_customers.clear()
 
-func _init(price):
+func update_marketing(new_marketing):
+	self.marketing = new_marketing
+
+#instantinate population with actual price
+func _init(price, marketing_value):
+	self.marketing = marketing_value
+	var amounts = get_amount_for_all(price)
+	init_active_customers(amounts, price)
+	
+	
 	
 
 # Called when the node enters the scene tree for the first time.
