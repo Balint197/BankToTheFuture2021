0b3ff1dfac4c3521bf79ca722a694b5471cea68e
Krisz98 <kkranitz98@gmail.com>
2021-03-06 12:05:19 +0100

    Simulation model done
    Main simulation frame created with state machine, Buffet class created

---
+++ Buffet.gd
@@ -0,0 +1,16 @@
+extends Object
+
+class_name Buffet
+
+var currentCustomerCount := 0
+var incomeDelta := 0.0
+var income :=0
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
