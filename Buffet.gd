ce90a278331c19a439b49e6d8202ead3c44cfd36
bcsabae <berky.csaba@outlook.com>
2021-03-07 12:52:59 +0100

    Added billing and reputation configuration

--- Buffet.gd
+++ Buffet.gd
@@ -2,6 +2,28 @@ extends Object
 
 class_name Buffet
 
+var RENT_OPTIONS = [
+	{'price': 0, 'satisfaction': 0},
+	{'price': 500, 'satisfaction': 0.1},
+	{'price': 1000, 'satisfaction': 0.4},
+	{'price': 2000, 'satisfaction': 0.5}
+]
+
+var CHEF_OPTIONS = [
+	{'price': 100, 'satisfaction': 0},
+	{'price': 200, 'satisfaction': 0.1},
+	{'price': 500, 'satisfaction': 0.3},
+	{'price': 1000, 'satisfaction': 0.5},
+]
+
+#100% reach costs this amount
+const MARKETING_PRICE = 1000
+
+#one material option caused satisfaction
+const RAW_MATERIAL_SATISFACTION = 0.1
+
+
+
 #--------------START OF CONSTANTS--------------------------------#
 const MAX_NUM_OF_CUSTOMERS_TO_DISPLAY = 4
 #--------------END OF CONSTANTS--------------------------------#
@@ -52,17 +74,32 @@ func removeCustomer():
 	return customers.pop_front()
 	
 func payBills():
-	var bills = \
-		self.rentedSpace + \
-		self.chefs + \
-		self.marketing	
+	var bills = 0
+	bills += RENT_OPTIONS[int(self.rentedSpace)]['price']
+	bills += CHEF_OPTIONS[int(self.chefs)-1]['price']
+	bills += self.marketing/100.0 * MARKETING_PRICE
 	print("Paying bills: {r} rent, {c} chef, {m} marketing".format({
-		'r': self.rentedSpace,
-		'c': self.chef,
-		'm': self.marketing
+		'r': RENT_OPTIONS[int(self.rentedSpace)]['price'],
+		'c': CHEF_OPTIONS[int(self.chefs)-1]['price'],
+		'm': self.marketing/100.0 * MARKETING_PRICE
 		}))	
 	self.overallIncome -= bills
 	return bills
-		
+	
+func getReputation():
+	var reputation = 1
+	reputation += RENT_OPTIONS[int(self.rentedSpace)]['satisfaction']
+	reputation += CHEF_OPTIONS[int(self.chefs)-1]['satisfaction']
+	reputation += self.rawMaterial * RAW_MATERIAL_SATISFACTION
+	print("Calculating reputation: {r} from rent, {c} from chef, {m} from material, all together {s}".format(
+		{
+			'r': RENT_OPTIONS[int(self.rentedSpace)]['satisfaction'],
+			'c': CHEF_OPTIONS[int(self.chefs)-1]['satisfaction'],
+			'm': self.rawMaterial * RAW_MATERIAL_SATISFACTION,
+			's': reputation
+		}
+	))
+	return reputation
+	
 
 
